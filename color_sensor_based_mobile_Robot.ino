dfc105a6f92b6e3128b54a339d037d555033b140
Kuljeet kaur Gill <142562400+Kuljeet2021@users.noreply.github.com>
2024-02-01 13:26:12 +0530

    Add files via upload

---
+++ color_sensor_based_mobile_Robot.ino
@@ -0,0 +1,126 @@
+const int s0 = 8; 
+const int s1 = 9;  
+const int s2 = 12;  
+const int s3 = 11;  
+const int out = 10;   
+// LED pins connected to Arduino
+int redLed = 2;  
+int greenLed = 3;  
+int blueLed = 4;
+
+// Variables  
+int red = 0;  
+int green = 0;  
+int blue = 0;  
+int IN1 = 6;
+
+int IN2 = 5;
+int IN3 = 13;
+int IN4 = 7;
+   
+void setup()   
+{  
+  Serial.begin(9600); 
+  pinMode(s0, OUTPUT);  
+  pinMode(s1, OUTPUT);  
+  pinMode(s2, OUTPUT);  
+  pinMode(s3, OUTPUT);  
+  pinMode(out, INPUT);  
+  pinMode(redLed, OUTPUT);  
+  pinMode(greenLed, OUTPUT);  
+  pinMode(blueLed, OUTPUT);  
+
+  digitalWrite(s0, HIGH);  
+  digitalWrite(s1, HIGH);  
+
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+}  
+    
+void loop(){  
+   digitalWrite(redLed, LOW); // Turn RED LED ON 
+   digitalWrite(greenLed, LOW);  
+   digitalWrite(blueLed, LOW); 
+   
+  color(); 
+  Serial.print("R Intensity:");  
+  Serial.print(red, DEC);  
+  Serial.print(" G Intensity: ");  
+  Serial.print(green, DEC);  
+  Serial.print(" B Intensity : ");  
+  Serial.print(blue, DEC);  
+  //Serial.println();  
+  
+if (red < blue && red < green && red < 20)
+///if ((red < 11  && red >4) && ( blue < 33 && green > 13 ) && ( green <38 && green > 18 ) )
+  { 
+  Serial.println(" - (Red Color)");  
+   digitalWrite(redLed, HIGH); // Turn RED LED ON 
+  digitalWrite(greenLed, LOW);  
+  digitalWrite(blueLed, LOW);  
+
+   // Stop robot
+ digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  delay (2000);
+   
+ }  
+
+else if (blue < red && blue < green)   
+
+//if ((red < 40  && red >38 ) && ( blue < 36 && green > 34 ) && ( green < 52 && green > 50 ) )
+  {  
+   Serial.println(" - (Blue Color)");  
+   digitalWrite(redLed, LOW);  
+   digitalWrite(greenLed, LOW);  
+   digitalWrite(blueLed, HIGH); // Turn BLUE LED ON  
+
+// robot moves forward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  }  
+
+else if (green < red && green < blue)  
+//else if ((red < 56 && red >30) && ( blue < 35 && green > 25 ) && ( green < 40 && green > 23 ) )
+  {  
+   Serial.println(" - (Green Color)");  
+   digitalWrite(redLed, LOW);  
+   digitalWrite(greenLed, HIGH); // Turn GREEN LED ON 
+   digitalWrite(blueLed, LOW);  
+// move backward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay (2000);
+  }  
+ 
+  else{
+  Serial.println();  
+  }
+  delay(300);   
+  digitalWrite(redLed, LOW);  
+  digitalWrite(greenLed, LOW);  
+  digitalWrite(blueLed, LOW);  
+ }  
+    
+void color()  
+{    
+  digitalWrite(s2, LOW);  
+  digitalWrite(s3, LOW);  
+  //count OUT, pRed, RED  
+  red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
+  digitalWrite(s3, HIGH);  
+  //count OUT, pBLUE, BLUE  
+  blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
+  digitalWrite(s2, HIGH);  
+  //count OUT, pGreen, GREEN  
+  green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);  
+}
